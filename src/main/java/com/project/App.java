07943d6cb7fb649634deefb62c9413184fac3d19
Kolupula Raju <rkolupula589@gmail.com>
2026-02-06 15:57:35 +0530

    Update App.java

--- src/main/java/com/project/App.java
+++ src/main/java/com/project/App.java
@@ -1,9 +1,18 @@
 package com.project;
-import java.util.ResourceBundle;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Application starting...Raju");
         
+        // This loop is the "heartbeat" that keeps the Pod running
+        try {
+            while (true) {
+                System.out.println("Pod is active and waiting for Grafana to scrape metrics...");
+                // Sleep for 30 seconds so we don't spam the logs
+                Thread.sleep(30000); 
+            }
+        } catch (InterruptedException e) {
+            System.err.println("Application interrupted: " + e.getMessage());
+        }
     }
 }
