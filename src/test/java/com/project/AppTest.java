120780b93d14ea63b8c641ee92f72090b7f2ab57
Kolupula Raju <rkolupula589@gmail.com>
2026-02-06 13:10:40 +0530

    Update AppTest.java

--- src/test/java/com/project/AppTest.java
+++ src/test/java/com/project/AppTest.java
@@ -1,34 +1,18 @@
 package com.project;
 
-import java.util.Scanner;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-public class App {
-
-    // The business logic required by your TestNG tests
-    public int userLogin(String username, String password) {
-        // Logic: Returns 1 for success if password contains '@', else 0
-        // (Based on your AppTest.java testLogin2 expecting 1 for "abc@1234")
-        if (username.equals("abc") && password.contains("@")) {
-            return 1;
-        }
-        return 0;
+public class AppTest {
+    @Test
+    public void testLogin1() {
+        App myApp = new App();
+        Assert.assertEquals(0, myApp.userLogin("abc", "abc1234"));
     }
 
-    // The entry point required by Kubernetes/JVM to prevent CrashLoopBackOff
-    public static void main(String[] args) {
-        System.out.println("--- Java Application Starting ---");
-        
-        // This simple loop keeps the pod running so Prometheus can scrape it
+    @Test
+    public void testLogin2() {
         App myApp = new App();
-        System.out.println("Application is now live and waiting for requests...");
-        
-        // Prevent the container from exiting immediately
-        try {
-            while (true) {
-                Thread.sleep(10000); 
-            }
-        } catch (InterruptedException e) {
-            System.out.println("Application Interrupted.");
-        }
+        Assert.assertEquals(1, myApp.userLogin("abc", "abc@1234"));
     }
 }
