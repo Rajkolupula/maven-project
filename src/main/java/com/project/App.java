c1eadff1a171dc8c66f3c51a76dfe47761c581ab
Kolupula Raju <rkolupula589@gmail.com>
2026-02-06 15:48:26 +0530

    Update App.java

--- src/main/java/com/project/App.java
+++ src/main/java/com/project/App.java
@@ -3,19 +3,7 @@ import java.util.ResourceBundle;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Application starting...");
-        try {
-            // Test the resource bundle immediately to catch errors
-            ResourceBundle rb = ResourceBundle.getBundle("config");
-            System.out.println("Config loaded! Username: " + rb.getString("username"));
-            
-            // Keep the pod running for Grafana metrics
-            while (true) {
-                Thread.sleep(10000);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(1); // Force a crash so you can see the error in logs
-        }
+        System.out.println("Application starting...Raju");
+        
     }
 }
